beec6abaa8085c4e5daf536e435e88104ac775ce
Svpernova09 <halo09@gmail.com>
2013-06-30 15:05:13 -0500

    Adding basic config info

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 rfid-php-crud
+
+Installation
+===
+Rename config-sample.php to config.php
+Set the path to your sqlite database in $database_path
---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: halo
+ * Date: 6/30/13
+ * Time: 2:51 PM
+ * 
+ */
+
+$database_path = '';
