6f0541ea409d269908f29224cb2d0850b0506a93
Svpernova09 <halo09@gmail.com>
2013-06-30 17:06:12 -0500

    login form functionality

--- login.php
+++ login.php
@@ -8,4 +8,31 @@
  */
 //show login form
 
-//process login
+?>
+<div class="login-wrapper">
+    <h1>Log in</h1>
+    <form name="login_form" id="login_form">
+        <input type="text" name="pin" id="pin" placeholder="PIN">
+        <input type="password" name="password" id="password" placeholder="password">
+        <input type="submit" name="submit" id="submit" value="submit">
+    </form>
+</div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#login_form').submit(function(e){
+            e.preventDefault();
+            //Ajax request to save form
+            var fields = $(this).serialize();
+            $.ajax({
+                url: 'ajax/login-processing.php',
+                dataType: 'json',
+                type: 'POST',
+                data: fields,
+                success: function(data){
+                    window.location = "/admin.php;
+
+                }
+            });
+        });
+    })
+</script>
