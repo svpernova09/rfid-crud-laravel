7ec97e0cd38dd3897fec79e2b744d9e87c3c5fd2
Svpernova09 <halo09@gmail.com>
2013-07-01 17:21:45 -0500

    edit functionality working. ready for initial testing.

--- admin.php
+++ admin.php
@@ -8,42 +8,156 @@
  */
 // Admin Functions
 include(dirname(__FILE__) . '/config/config.php');
-$key = filter_var($_POST['pin'], FILTER_SANITIZE_STRING);
-$password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+if(isset($_POST['pin'])){
+    $key = filter_var($_POST['pin'], FILTER_SANITIZE_STRING);
+}
+if(isset($_POST['password'])){
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+}
+if(isset($_GET['action'])){
+    $action = filter_var($_GET['action'], FILTER_SANITIZE_STRING);
+}
+if(isset($_GET['key'])){
+    $key = filter_var($_GET['key'], FILTER_SANITIZE_STRING);
+}
+if(isset($_GET['rowid'])){
+    $rowid = filter_var($_GET['rowid'], FILTER_SANITIZE_STRING);
+}
 require_once(dirname(__FILE__) . '/lib/autoloader.php');
 $crypto = new Crypto();
-try{
-    //open the database
-    $db = new PDO('sqlite:' . $database_path . $database_name);
-    $params = array(':key' => $key);
-    $query = "SELECT * FROM users WHERE key = :key";
-    $rows = $db->prepare($query);
-    $rows->execute($params);
-    $data = $rows->fetchall();
-    // close the database connection
-    $errors = $db->errorInfo();
-    $db = NULL;
-    $stored_hash = $data['0']['hash'];
-} catch(PDOException $e) {
-    print 'Exception : '.$e->getMessage();
+if(!$_COOKIE['logged_in']){
+    try{
+        //open the database
+        $db = new PDO('sqlite:' . $database_path . $database_name);
+        $params = array(':key' => $key);
+        $query = "SELECT * FROM users WHERE key = :key";
+        $rows = $db->prepare($query);
+        $rows->execute($params);
+        $data = $rows->fetchall();
+        // close the database connection
+        $errors = $db->errorInfo();
+        $db = NULL;
+        $stored_hash = $data['0']['hash'];
+    } catch(PDOException $e) {
+        print 'Exception : '.$e->getMessage();
+    }
+    $stored = explode('$',$stored_hash);
+    $supplied_check = $crypto->CheckThis($password, $stored['2']);
+    $supplied = explode('$',$supplied_check);
+    $user_hash = $stored['3'];
+    $supplied_hash = $supplied['3'];
 }
-$stored = explode('$',$stored_hash);
-$supplied_check = $crypto->CheckThis($password, $stored['2']);
-$supplied = explode('$',$supplied_check);
-$user_hash = $stored['3'];
-$supplied_hash = $supplied['3'];
-if(($supplied_hash == $user_hash) && $data['0']['isAdmin']){
+if((($supplied_hash == $user_hash) && $data['0']['isAdmin']) || ($_COOKIE['logged_in'])){
     //user authenticated
-    //setcookie("logged_in", 1, time()+3600);  /* expires in 1 hour */
+    setcookie("logged_in", 1, time()+3600);  /* expires in 1 hour */
     ?>
-    Welcome to admin <?php if(isset($data['0']['ircName'])) { echo $data['0']['ircName']; } ?>
+    Welcome to admin <?php if(isset($data['0']['ircName'])) { echo $data['0']['ircName']; } ?><br>
     <?php
     $crud = new Crud();
-    $all_users = $crud->GetAll();
-    ?>
-    <pre><?php var_dump($all_users);?></pre>
-    <?php
+
+    switch($action){
+        case "add":
+            break;
+        case "edit":
+            $this_user = $crud->GetThisUser($key);
+            ?>
+            <pre><!-- <?php var_dump($this_user);?> --></pre>
+            <?php
+            if(count($this_user) == "1"){
+                //show edit form
+                ?>
+                <h2>Editing <?php echo $this_user['0']['ircName']; ?></h2>
+                <form id="edit_form" name="edit_form" method="POST" action="admin.php?action=doedit&rowid=<?php echo $this_user['0']['rowid']; ?>">
+                    <label for="key">Key: </label>
+                    <input type="text" name="key" id="key" value="<?php echo $this_user['0']['key']; ?>"><br />
+                    <label for="key">Hash: </label>
+                    <input type="text" name="hash" id="hash" value="<?php echo $this_user['0']['hash']; ?>"><br />
+                    <label for="key">IRC Name: </label>
+                    <input type="text" name="ircName" id="ircName" value="<?php echo $this_user['0']['ircName']; ?>"><br />
+                    <label for="key">Spoken Name: </label>
+                    <input type="text" name="spokenName" id="spokenName" value="<?php echo $this_user['0']['spokenName']; ?>"><br />
+                    <label for="key">Added By: </label>
+                    <input type="text" name="addedBy" id="addedBy" value="<?php echo $this_user['0']['addedBy']; ?>"><br />
+                    <label for="key">Date Created: </label>
+                    <input type="text" name="dateCreated" id="dateCreated" value="<?php echo $this_user['0']['dateCreated']; ?>"><br />
+                    <label for="key">Is Admin: </label>
+                    <input type="text" name="isAdmin" id="isAdmin" value="<?php echo $this_user['0']['isAdmin']; ?>"><br />
+                    <label for="key">Last Login: </label>
+                    <input type="text" name="lastLogin" id="lastLogin" value="<?php echo $this_user['0']['lastLogin']; ?>"><br />
+                    <label for="key">Is Active: </label>
+                    <input type="text" name="isActive" id="isActive" value="<?php echo $this_user['0']['isActive']; ?>"><br />
+
+                    <input type="submit" name="submit" id="submit" value="submit">
+
+                </form>
+                <?php
+            }
+            break;
+        case "doedit":
+
+            ?>
+            Run the edit
+            <?php
+            foreach($_POST AS $key => $value){
+                $_POST[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+            }
+            $data = array(':key'=>$_POST['key'],
+                            ':hash'=>$_POST['hash'],
+                            ':ircName'=>$_POST['ircName'],
+                            ':spokenName'=>$_POST['spokenName'],
+                            ':addedBy'=>$_POST['addedBy'],
+                            ':dateCreated'=>$_POST['dateCreated'],
+                            ':isAdmin'=>$_POST['isAdmin'],
+                            ':lastLogin'=>$_POST['lastLogin'],
+                            ':isActive'=>$_POST['isActive'],
+                            ':rowid'=>$rowid);
+            $errors = $crud->UpdateUser($data);
+            if($errors['status'] == 'success'){
+                ?>
+                Update Successful<BR>
+                <?php
+            } else {
+                $error_msg = "Failed to Update";
+                echo $error_msg . "<br />";
+                var_dump($errors);
+            }
+            break;
+        default:
+            $all_users = $crud->GetAll();
+            ?>
+                <pre><!-- <?php var_dump($all_users);?> --></pre>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>RFID Tag</td>
+                        <td>IRC Name</td>
+                        <td>Admin</td>
+                        <td>Active</td>
+                        <td>Edit</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach($all_users AS $user){
+                        ?>
+                        <tr>
+                            <td><?php echo $user['key']; ?></td>
+                            <td><?php echo $user['ircName']; ?></td>
+                            <td><?php echo $user['isAdmin']; ?></td>
+                            <td><?php echo $user['isActive']; ?></td>
+                            <td><a href="admin.php?action=edit&key=<?php echo $user['key']; ?>">Edit</a></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            <?php
+            break;
+    }
+
 } else {
     //login failed
     header('Location: /login.php');
 }
+?>
--- classes/Crud.php
+++ classes/Crud.php
@@ -15,15 +15,45 @@ class Crud extends Config {
     public function Create(){
 
     }
-    public function Update(){
+    public function UpdateUser($data){
+        $this->ext_conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $params = $data;
+        $update = $this->ext_conn->prepare("UPDATE users SET key = :key,
+                                                            hash = :hash,
+                                                            ircName = :ircName,
+                                                            spokenName = :spokenName,
+                                                            addedBy = :addedBy,
+                                                            dateCreated = :dateCreated,
+                                                            isAdmin = :isAdmin,
+                                                            lastLogin = :lastLogin,
+                                                            isActive = :isActive
+                                             WHERE rowid = :rowid");
 
+        $update->execute($data);
+
+        if ($update->errorCode() == '0000') {
+            return array('status' => 'success', 'rowid' => $params[':rowid']);
+        } else {
+            return array('status' => 'failure', 'reason' => 'update_failed','error' => $update->errorInfo());
+        }
     }
     public function Delete(){
 
     }
+    public function GetThisUser($key){
+        $params = array(':key' => $key);
+        $query = "SELECT rowid,* FROM users WHERE key = :key";
+        $rows = $this->ext_conn->prepare($query);
+        $rows->execute($params);
+        $data = $rows->fetchall();
+        // close the database connection
+        $errors = $this->ext_conn->errorInfo();
+        $db = NULL;
+        return $data;
+    }
     public function GetAll(){
         $params = array();
-        $query = "SELECT * FROM users WHERE 1";
+        $query = "SELECT rowid,* FROM users WHERE 1";
         $rows = $this->ext_conn->prepare($query);
         $rows->execute($params);
         $data = $rows->fetchall();
