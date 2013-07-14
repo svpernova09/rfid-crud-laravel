74b89005414cc287156ae990979005809dd45afe
Svpernova09 <halo09@gmail.com>
2013-07-14 17:02:24 -0500

    Updatiing instructions

--- config/config-sample.php
+++ config/config-sample.php
@@ -5,7 +5,11 @@
  * Date: 6/30/13
  * Time: 2:51 PM
  *
+ * $database_path MUST be writable by the user that accessing the database
+ *
  */
+
 $debug = 1;
 $database_path = '/'; // MUST end with a /
 $database_name = 'database.sqlite';
+require_once(dirname(__FILE__) . '/../lib/autoloader.php');
